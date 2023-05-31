e3b1113bb4902c46fb137ecf994a219e782b096c
Vitoria Tavares <vitoria@pop-os.localdomain>
2023-05-31 20:06:28 -0300

    start menu

---
+++ testemenu.py
@@ -0,0 +1,22 @@
+import pygame, sys
+from pygame.locals import * 
+
+pygame.init()
+
+altura_tela = 640
+largura_tela = 480
+
+SURFACE = pygame.display.set_mode((altura_tela, largura_tela))
+pygame.display.set_caption("menu")
+
+run = True
+while run:
+    SURFACE.fill((105,89,205))
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+
+    pygame.display.flip()
